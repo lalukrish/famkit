1070aaf0d0b714fd931b91fc44b993ba5dba73a4
Lal krishna N C <lalukrishnavallil01@gmail.com>
2023-02-09 10:50:20 +0530

    db connected

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
 const express = require("express");
 const app = express();
 const PORT = 7000;
+const mongoose = require("mongoose");
 
 app.get("/", (req, res) => {
   res.send("hei");
+});
+
+mongoose.connect(process.env.MONGOURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("connected");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log("err connect", err);
 });
 
 app.listen(PORT, () => {
